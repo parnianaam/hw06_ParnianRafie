386c03bf9ae21586bb34ff99bc33d80f1acc3b9a
parnianaam <parnian.rafie58@gmail.com>
2023-01-14 20:42:03 +0330

    this is final project

---
+++ homework_06_parnianrafie/Desirialize.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework6_parnianRafie
+{
+    public static class Desirialize
+    {
+
+
+
+        public static List<User> userList()
+        {
+            List<User> users = new List<User>();
+            string DataStorageFile = File.ReadAllText(@"C:\Users\hufu\Desktop\homework_06_parnianrafie\DataStorage.csv");
+            string[] DataStorageFileArray = DataStorageFile.Split("\n");
+            foreach (var fileLine in DataStorageFileArray)
+            {
+                var DataStorageObject = JsonConvert.DeserializeObject<User>(fileLine);
+                if (DataStorageObject != null)
+                {
+                    users.Add(DataStorageObject);
+                }
+
+            }
+            return users;
+        }
+
+    }
+}
